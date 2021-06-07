f02c1baf8e456837936b65f450a0c6b6d9564472
y <yared-haake@web.de>
2021-06-07 17:41:30 +0200

    Changed line endings to LF

--- include/applicationLayer.h
+++ include/applicationLayer.h
@@ -3,10 +3,10 @@
 /*
        Wenn Result.error_code = SUCCESS steht im Result.value der Wert den der
        Client auf der Konsole sehen soll.
-       Ansonsten wird nur der error_code im Result übermittelt und das value
+       Ansonsten wird nur der error_code im Result ï¿½bermittelt und das value
        bleibt LEER.
 */
-//Entscheidet ob und welcher Befehl ausgeführt wird, gibt Result an ClientSession zurück. Mögliche Error: COMMAND_NOT_FOUND
+//Entscheidet ob und welcher Befehl ausgefï¿½hrt wird, gibt Result an ClientSession zurï¿½ck. Mï¿½gliche Error: COMMAND_NOT_FOUND
 Result executeCommand(Command command);
 
 #endif // APPLICATION_LAYER_H_INCLUDED
